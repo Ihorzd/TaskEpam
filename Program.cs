0d14b1de631abf8742f23d8bcd9bbef14fb05d7a
Ihorzd <igorzdebskij@gmail.com>
2019-08-20 15:52:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task2Epam
+{
+    abstract class Figure
+    {
+        public abstract void Draw();
+        public readonly double X=3;
+        public readonly double Y=3;
+        public void figura(double x, double y)
+        {
+            x = this.X;
+            y = this.Y;
+        }
+        
+    }
+  
+    class Rectangle : Figure
+    {
+   
+        public override void Draw()
+        {
+            Console.WriteLine("Class name: Rectangle");
+        }
+    }
+    class Square : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Class name: Square");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle one = new Rectangle();
+            one.Draw();
+            Square two = new Square();
+            two.Draw();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
